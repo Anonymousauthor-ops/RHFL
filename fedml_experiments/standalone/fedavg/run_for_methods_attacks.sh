24079ce7d57590b18aac7ce4c2773a607fd43adc
Anonymousauthor-ops <40414686+Anonymousauthor-ops@users.noreply.github.com>
2024-04-10 21:06:55 +0800

    Update run_for_methods_attacks.sh

--- fedml_experiments/standalone/fedavg/run_for_methods_attacks.sh
+++ fedml_experiments/standalone/fedavg/run_for_methods_attacks.sh
@@ -5,12 +5,12 @@ attacks=("label" "sign" "gaussian" "krum_attack" "full_trim" )
 for attack in "${attacks[@]}"; do
     for method in "${methods[@]}"; do
         echo "Execute method = ${method}，attack=${attack}"
-        for i in 5; do  # control malicious clients number
+        for i in 30; do  # control malicious clients number
             echo " m=${i} is starting"
             echo "Execute now-"
-            sh run_fedavg_standalone_pytorch.sh 0 100 10 8 mnist "mnist" cnn hetero 1 1 0.001 sgd 0 500 ${i} ${method} ${attack}
+            sh run_fedavg_standalone_pytorch.sh 0 100 10 8 mnist "mnist" cnn hetero 100 10 0.001 sgd 0 500 ${i} ${method} ${attack}
             # if you want to run on cifar10, please change relevant parameters
-            #sh run_fedavg_standalone_pytorch.sh 0 100 10 10 cifar10 "cifar10" resnet20 hetero 1 1 0.001 sgd 0 500 ${i} ${method} ${attack}
+            #sh run_fedavg_standalone_pytorch.sh 0 100 10 10 cifar10 "cifar10" resnet20 hetero 100 10 0.001 sgd 0 500 ${i} ${method} ${attack}
         done
     done
 done
